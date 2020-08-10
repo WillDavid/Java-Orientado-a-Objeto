291914da35ad9c89a4c92f2612ec7415e8146c49
lucas3213 <ltn.lic17@uea.edu.br>
2020-08-10 18:59:46 -0300

    Exercicios PP de ArrayList OO

---
+++ Main.java
@@ -0,0 +1,42 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nome;
+		int sorteado;
+		
+		Pessoa[] pessoas = new Pessoa[4];
+		
+		for(int i=0; i< pessoas.length; i++) {
+			
+			pessoas[i] = new Pessoa();
+			
+			nome = JOptionPane.showInputDialog("Informe o nome:");
+			pessoas[i].setNome(nome);
+			pessoas[i].setNumeroParaSorteio(i);
+			
+			
+		}
+		
+		
+		for(int j = 0; j< pessoas.length; j++) {
+			System.out.println(pessoas[j].getNome());
+		}
+		
+		sorteado = (int)(Math.random() * pessoas.length);
+		
+		
+		for(int i=0; i<pessoas.length; i++) {
+			if(pessoas[i].getNumeroParaSorteio() == sorteado) {
+				JOptionPane.showMessageDialog(null, "A pessoa sorteada:" + pessoas[i].getNome());
+			}
+		}
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Pessoa.java
@@ -0,0 +1,21 @@
+
+public class Pessoa {
+	private String nome;
+	private int numeroParaSorteio;
+
+	public int getNumeroParaSorteio() {
+		return numeroParaSorteio;
+	}
+
+	public void setNumeroParaSorteio(int numeroParaSorteio) {
+		this.numeroParaSorteio = numeroParaSorteio;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
